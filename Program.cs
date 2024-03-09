5a804a5f80e7898fe9245efef051bc314262fe2c
sara777b <162568828+sara777b@users.noreply.github.com>
2024-03-09 12:40:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomaciSB1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int prvi, drugi, rezultats, rezultato, rezultatm, rezultatd, rezultatostatak;
+            string stringPrvi, stringDrugi;
+
+            Console.Write("Unesite prvi broj: ");
+            stringPrvi = Console.ReadLine();
+            Console.Write("Unesite drugi broj: ");
+            stringDrugi = Console.ReadLine();
+
+            prvi = int.Parse(stringPrvi);
+            drugi = int.Parse(stringDrugi);
+
+            rezultats = prvi + drugi;
+            rezultato = prvi - drugi;
+            rezultatm = prvi * drugi;
+            rezultatd = prvi / drugi;
+            rezultatostatak = prvi % drugi;
+
+            Console.WriteLine(prvi + " " + "+" + " " + drugi + " " + "=" + " " + rezultats);
+            Console.WriteLine(prvi + " " + "-" + " " + drugi + " " + "=" + " " + rezultato);
+            Console.WriteLine(prvi + " " + "*" + " " + drugi + " " + "=" + " " + rezultatm);
+            Console.WriteLine(prvi + " " + "/" + " " + drugi + " " + "=" + " " + rezultatd);
+            Console.WriteLine(prvi + " " + "ostatak" + " " + drugi + " " + "=" + " " + rezultatostatak);
+            Console.ReadKey();
+
+
+        }
+    }
+}
